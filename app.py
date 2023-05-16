a686ac347f0cc27baf0a1a642ddb4ae9a6cfaa24
Chris <chriswu.ca@gmail.com>
2023-05-16 15:48:29 -0700

    update app.py

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 from flask import Flask, request, render_template
-from sqlalchemy.orm import scoped_session
-from sqlalchemy import create_engine
-from database import SessionLocal, Base
-from models import Patient
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+db = SQLAlchemy(app)
 
-# Set up database
-engine = create_engine("postgresql://postgres:postgres@db/your_database")
-db_session = scoped_session(SessionLocal, scopefunc=_app_ctx_stack.__ident_func__)
+class Patient(db.Model):
+    __tablename__ = 'patients'
 
-@app.teardown_appcontext
-def cleanup(resp_or_exc):
-    db_session.remove()
+    id = db.Column(db.String(36), primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    gender = db.Column(db.String(1), nullable=False)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        patient_id = uuid.uuid4()
+db.create_all()
+
+# create a registration form
+@app.route('/register', methods=['GET'])
+def register_form():
+    return render_template('register.html')
+
+# create a patient
+@app.route('/register', methods=['POST'])
+def register_patient():
+    try:
+        patient_id = str(uuid.uuid4())
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
         email = request.form.get('email')
@@ -25,7 +35,7 @@ def register():
         gender = request.form.get('gender')
 
         new_patient = Patient(
-            id=str(patient_id),
+            id=patient_id,
             first_name=first_name,
             last_name=last_name,
             email=email,
@@ -33,12 +43,21 @@ def register():
             gender=gender
         )
 
-        db_session.add(new_patient)
-        db_session.commit()
+        db.session.add(new_patient)
+        db.session.commit()
 
         return 'Registration Successful'
-    return render_template('register.html')
+    except Exception as e:
+        return f'Error creating patient: {str(e)}'
+
+# get all patients
+@app.route('/patients', methods=['GET'])
+def get_patients():
+    try:
+        patients = Patient.query.all()
+        return render_template('patients.html', patients=patients)
+    except Exception as e:
+        return f'Error getting patients: {str(e)}'
 
 if __name__ == '__main__':
-    Base.metadata.create_all(bind=engine)
     app.run(debug=True, host='0.0.0.0')
